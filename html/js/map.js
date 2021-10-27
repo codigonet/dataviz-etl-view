f80b4c2bb2a3915741fe214f435279b32d618197
Cristian Acuña <cas@codigonet.net>
2021-10-26 22:31:27 -0300

    feat: evaluación de datos para mostrar colores en marcadores

--- html/js/map.js
+++ html/js/map.js
@@ -21,23 +21,35 @@ if (mapboxgl) {
 
         // Agregar Marcadores.
         let communes_keys = Object.keys(json_data);
+        // communes_keys = ["VALPARAISO", "PETORCA", "LA LIGUA", "QUILLOTA", "LOS ANDES"]
         for (const communes of communes_keys) {
             // Create a DOM element for each marker.
             const el = document.createElement('div');
+            // <div class="marker" style="border-radius...></div>
             const width = 25;
             const height = 25;
             el.className = 'marker';
             el.style.borderRadius = "5px";
-            el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`;
+            //el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`;
             el.style.width = `${width}px`;
             el.style.height = `${height}px`;
             el.style.backgroundSize = '100%';
+            el.style.backgroundColor = 'green';
+            /*
+            if(json_data[communes].DATA["GASTO (M$)"] > json_data[communes].DATA["SALDO (M$)"]){
+                el.style.backgroundColor = 'red';
+            }
+            */
+            if (json_data[communes].DATA["SALDO (M$)"] < 14000000) {
+                el.style.backgroundColor = 'red';
+            }
 
             el.addEventListener('click', () => {
-                let gasto = parseInt(json_data[communes].DATA["GASTO (M$)"]); 
+                let gasto = parseInt(json_data[communes].DATA["GASTO (M$)"]);
                 let msg = `
                 La comuna de ${json_data[communes].DATA.COMUNA}, \n
-                tiene un gasto acumulado de: ${gasto}
+                tiene un gasto acumulado de: ${gasto}, \n
+                La suma de 1 más 1 es: ${(1 + 1)}.
                 `;
                 window.alert(msg);
             });
